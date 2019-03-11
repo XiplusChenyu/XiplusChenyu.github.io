ac9afea9c2ef7cde48f1226f9dd2ed25ce3bd5f8
Chenyu Xi <xi_chenyu@163.com>
2019-03-11 17:17:53 -0400

    add chatbot href

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
+/**
+ * @return {string}
+ */
+function PrefixInteger(num, n) {
+    return (Array(n).join(0) + num).slice(-n);
+}
 
 
 function showTime(){
     let date = new Date();
     // noinspection JSAnnotator
-    let year = date.getFullYear();
-    let month = date.getMonth();
-    let d = date.getDate();
-    let h=date.getHours();
-    let m=date.getMinutes();
+    let year = PrefixInteger(date.getFullYear(), 4);
+    let month = PrefixInteger(date.getMonth(), 2);
+    let d = PrefixInteger(date.getDate(),2);
+    let h=PrefixInteger(date.getHours(),2);
+    let m=PrefixInteger(date.getMinutes(),2);
+
 
     document.getElementById('timeStamp').innerHTML = ` ${d}-${month}-${year} ${h}:${m}  `;
     setTimeout(showTime,60000);
 }
+
 
 function clickshow(id){
     let element = document.getElementById(id);
